92b20cf4fba5f05062ee768a46afbdd9f4a37959
sakusaku3939 <yuukiyuuki223@gmail.com>
2020-07-18 05:36:28 +0900

    ログインフォームの実装

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const http = require('http');
 const auth = require('http-auth');
 const router = require('./lib/router');
 
-const basic = auth.basic({
-  realm: 'Enter username and password.',
-  file: './users.htpasswd'
-});
+// const basic = auth.basic({
+//   realm: 'Enter username and password.',
+//   file: './users.htpasswd'
+// });
 
 const server = http
-  .createServer(basic, (req, res) => {
+  .createServer((req, res) => {
     router.route(req, res);
   })
   .on('error', e => {
